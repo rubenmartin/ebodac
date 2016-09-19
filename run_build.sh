a6a3c3a07b206a35b86a99e5473276674b7cade1
rubenmartin <rmartinramos@soldevelo.com>
2016-09-19 11:27:57 +0200

    EBODAC-1089- added config_services.sh updated (19-09-2016)

--- run_build.sh
+++ run_build.sh
@@ -2,9 +2,7 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     if [ "$DB" = "mysql" ]; then
-        echo "USE mysql;\nUPDATE user SET password=PASSWORD('password') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
+        echo "USE mysql;\nUPDATE user SET password=PASSWORD('$LOCAL_MYSQL_PASSWORD') WHERE user='$LOCAL_MYSQL_USER';\nFLUSH PRIVILEGES;\n" | mysql -u root
         mvn clean install -PIT -U
-    elif [ "$DB" = "psql" ]; then
-        mvn -Dmotech.sql.password=password -Dmotech.sql.user=postgres -Dmaven.test.failure.ignore=false -Dmotech.sql.driver=org.postgresql.Driver -Dmotech.sql.dbtype=psql -Dmotech.sql.url=jdbc:postgresql://localhost:5432/ clean install -PIT -U
     fi
 fi

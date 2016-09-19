07b094c5e0aef413c01a138780d60c5de23cc470
rubenmartin <rmartinramos@soldevelo.com>
2016-09-19 13:34:14 +0200

    EBODAC-1089- fixed modified run_build.sh (19-09-2016)

--- run_build.sh
+++ run_build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-echo "USE mysql;\nUPDATE user SET password=PASSWORD('$LOCAL_MYSQL_PASSWORD') WHERE user='$LOCAL_MYSQL_USER';\nFLUSH PRIVILEGES;\n" | mysql -u root
-mvn clean install -PIT -U
-mvn  install -o -Pchrome-remote  -Dserver.url=$STAGING_SERVER -Dlogin.username=$MOTECH_USER -Dlogin.password=$MOTECH_STAGING_PASS -Dadmin.login=$EBODAC_L1_ADMIN_USER  -Dadmin.password=$EBODAC_L1_ADMIN_PASS -Danalyst.login=$EBODAC_L1_ANALYST_USER -Danalyst.password=$EBODAC_L1_ANALYST_PASS
+echo "USE mysql;\nUPDATE user SET password=PASSWORD('$LOCAL_MYSQL_PASSWORD') WHERE user='$LOCAL_MYSQL_USER';\nFLUSH PRIVILEGES;\n" | mysql -u $LOCAL_MYSQL_USER
+mvn clean install -PIT -U -X
+# mvn  install -o -Pchrome-remote  -Dserver.url=$STAGING_SERVER -Dlogin.username=$MOTECH_USER -Dlogin.password=$MOTECH_STAGING_PASS -Dadmin.login=$EBODAC_L1_ADMIN_USER  -Dadmin.password=$EBODAC_L1_ADMIN_PASS -Danalyst.login=$EBODAC_L1_ANALYST_USER -Danalyst.password=$EBODAC_L1_ANALYST_PASS
+
 

d291e4f3d9784af4d9f618ae82a5e238460e9a1b
rubenmartin <rmartinramos@soldevelo.com>
2016-09-19 17:26:06 +0200

    EBODAC-1089- fixed modified back run_build.sh (19-09-2016)

--- run_build.sh
+++ run_build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "USE mysql;\nUPDATE user SET password=PASSWORD('$LOCAL_MYSQL_PASSWORD') WHERE user='$LOCAL_MYSQL_USER';\nFLUSH PRIVILEGES;\n" | mysql -u $LOCAL_MYSQL_USER
-mvn clean install -X
+mvn clean install -PIT -U -X

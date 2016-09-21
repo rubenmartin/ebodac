f95e39c1e19b0b555bbfaa79ca0175951afe2452
rubenmartin <rmartinramos@soldevelo.com>
2016-09-21 17:24:11 +0200

    adding changes for the Travis (21-09-2016)

--- run_build.sh
+++ run_build.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-
 echo "USE mysql;\nUPDATE user SET password=PASSWORD('$LOCAL_MYSQL_PASSWORD') WHERE user='$LOCAL_MYSQL_USER';\nFLUSH PRIVILEGES;\n" | mysql -u $LOCAL_MYSQL_USER
 mvn clean install -PIT -U -X
